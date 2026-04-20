3a3f14a54af0915dcfe4c3f2c8700d3a81c6fbfe
Megan Barber <167549801+mbarber23@users.noreply.github.com>
2026-04-20 15:54:23 -0400

    Update Special_Education.sql
    update to correct data

--- Special_Education.sql
+++ Special_Education.sql
@@ -1,26 +1,21 @@
 /*This table queries the special education in Skyward (not PSSP). It pulls the students most resent SPED record. */
 
-
-WITH MaxDate AS (
-    SELECT
-        Enrollment.StudentID,
-        MAX(Enrollment.StartDate) AS MaxStartDate
-    FROM Student.SpecialEducation.SpecialEducationEnrollment AS Enrollment
-    GROUP BY Enrollment.StudentID
-)
 SELECT 
-    Enrollment.StudentID,
-    Enrollment.StartDate,
-    Enrollment.EndDate,
-    Program.DisabilityServiceProgramINID,
-    Program.Rank,
-    Disability.Code,
-    Disability.Description
-FROM Student.SpecialEducation.SpecialEducationEnrollment AS Enrollment
-JOIN MaxDate 
-    ON Enrollment.StudentID = MaxDate.StudentID 
-    AND Enrollment.StartDate = MaxDate.MaxStartDate
-JOIN Student.SpecialEducation.EnrollmentDisabilityServiceProgramIN AS Program 
-    ON Enrollment.SpecialEducationEnrollmentID = Program.EnrollmentINID
-JOIN Student.SpecialEducation.DisabilityServiceProgramIN AS Disability 
-    ON Program.DisabilityServiceProgramINID = Disability.DisabilityServiceProgramINID;
+SPEDIN.StudentID,
+SPEDIN.StartDate,
+SPEDIN.EndDate,
+SPEDDisability.Rank,
+SPEDProgram.Code,
+SPEDProgram.Description
+
+
+From Student.SpecialEducation.EnrollmentIN SPEDIN
+
+Join Student.SpecialEducation.EnrollmentDisabilityServiceProgramIN SPEDDisability 
+    ON SPEDIN.EnrollmentINID = SPEDDisability.EnrollmentINID
+
+Join Student.SpecialEducation.DisabilityServiceProgramIN SPEDProgram
+    ON SPEDDisability.DisabilityServiceProgramINID = SPEDProgram.DisabilityServiceProgramINID
+
+Where SpedIN.EndDate is null
+And SPEDIN.StudentID = 376607
